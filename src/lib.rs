e8db2338ac342e4bbb030f517531e540dcbb472b
yugure <ikeyanagi@outlook.jp>
2022-08-21 03:04:14 +0900

    create anchor-0.24.0 branch

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 anchor_gen::generate_cpi_interface!(
   idl_path = "src/artifacts/idl.json",
-  target_anchor_version = "",
+  target_anchor_version = "0.24.0",
   zero_copy(TickArray, Tick),
   packed(TickArray, Tick)
 );
